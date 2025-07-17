08226f89084fd3254202edc908a4869c3aec7a1a
Bret Scarlavai <bscarlavai@gmail.com>
2025-07-17 00:42:51 -0400

    fix another error

--- app/states/[state]/page.tsx
+++ app/states/[state]/page.tsx
@@ -221,7 +221,7 @@ export default async function StatePage({ params }: PageProps) {
                 </Link>
               </h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {locationsByCity[city].map((location) => (
+                {locationsByCity[city].map((location: any) => (
                   <LocationCard
                     key={location.id}
                     id={location.id}
